03210a95b5826af2e8036ec7b742a7d881942d8f
Shawn M Carlson <157416132+OxMagnus@users.noreply.github.com>
2024-02-25 12:05:41 -0500

    Final edits

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # Random Password Generator
 
 ## URL
+https://oxmagnus.github.io/Challenge-3-Random-Password-Generator/
 
 ##
 
 ## Contents
 This website allows the user to create a random password that has a minimum of 8 characters but no more then 128 characters. It will also prompts the user as to which characters they would like to include in their random password (Minimum of at least 1 character type is required). 
 ##
-
-## Usage 
-
 
 ## Contact
 
--- script.js
+++ script.js
@@ -1,55 +1,53 @@
 // Assignment code here
 
-function generatePassword() {
-
-     const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-     const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-     const numbers = "0123456789";
-     const symbols = "!@#$%^&*";
- 
-     let passwordLength = 8;
- 
-     const includeUppercase = confirm("Include uppercase letters?");
-     const includeLowercase = confirm("Include lowercase letters?");
-     const includeNumbers = confirm("Include numbers?");
-     const includeSymbols = confirm("Include symbols?");
-
-     let letters = "";
-     if (includeUppercase) letters += uppercaseLetters;
-     if (includeLowercase) letters += lowercaseLetters;
-     if (includeNumbers) letters += numbers;
-     if (includeSymbols) letters += symbols;
- 
-     if (letters === "") {
-       alert("Please select at least one character type");
-       return;
-     }
- 
-     do {
-       const length = prompt("Enter the length of the password (between 8 and 128 characters):");
-       passwordLength = parseInt(length);
- 
-       if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-         alert("Please enter a password length between 8 and 128 characters.");
-       }
-     } while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128);
- 
-
-
-     let password = "";
-     for (let i = 0; i < passwordLength; i++) {
-       const randomIndex = Math.floor(Math.random() * letters.length);
-       password += letters.charAt(randomIndex);
-     }
- 
-     document.getElementById("password").innerText = "Your password is: " + password
-   }
- 
-   generatePassword();
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+  const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*";
+
+  let passwordLength = 8;
+
+  const includeUppercase = confirm("Include uppercase letters?");
+  const includeLowercase = confirm("Include lowercase letters?");
+  const includeNumbers = confirm("Include numbers?");
+  const includeSymbols = confirm("Include symbols?");
+
+  let letters = "";
+  if (includeUppercase) letters += uppercaseLetters;
+  if (includeLowercase) letters += lowercaseLetters;
+  if (includeNumbers) letters += numbers;
+  if (includeSymbols) letters += symbols;
+
+  if (letters === "") {
+    alert("Please select at least one character type");
+    return;
+  }
+
+  do {
+    const length = prompt("Enter the length of the password (between 8 and 128 characters):");
+    passwordLength = parseInt(length);
+
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+      alert("Please enter a password length between 8 and 128 characters.");
+    }
+  } while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128);
+
+
+
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    password += letters.charAt(randomIndex);
+  }
+
+  return password
+
+}
 
 // Write password to the #password input
 function writePassword() {
